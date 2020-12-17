896228c548d7a86c53841910d82097e76a28a761
LutenkoMikhail <lamprologus2011@gmail.com>
2020-12-17 20:03:07 +0200

    Add LoadFixture

--- tests/Controller/TagControllerTest.php
+++ tests/Controller/TagControllerTest.php
@@ -4,23 +4,30 @@ namespace App\Tests\Controller;
 
 use App\DataFixtures\NewsFixtures;
 use App\DataFixtures\TagFixtures;
-use App\Tests\LoadFixtures;
+use App\Tests\FixtureAwareTestCase;
 
-class TagControllerTest extends LoadFixtures
+class TagControllerTest extends FixtureAwareTestCase
 {
+    protected $client;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->bootedKernel = static::bootKernel();
+        $this->client = static::$container->get('test.client');
+        $this->client->disableReboot();
+
+        $this->addFixture(new TagFixtures());
+        $this->addFixture(new NewsFixtures());
+        $this->executeFixtures();
+    }
 
     public function testIndexTags()
     {
-        $this->loadFixtures([
-            TagFixtures::class,
-            NewsFixtures::class,
-        ]);
 
-        $client = static::createClient();
-
-        $client->request('GET', '/api/v1/tags');
-
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->client->request('GET', '/api/v1/tags');
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
     }
 
 }
